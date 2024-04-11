ba622438719665204a7af4c0519053c5f1c6c71c
r4smusjpg <r4smusjpg@gmail.com>
2024-04-11 18:53:51 +0300

    create Processes::DataService

---
+++ app/services/processes/DataService.rb
@@ -0,0 +1,31 @@
+module Processes
+  class DataService
+    def self.call(pid)
+      self.new(pid)
+    end
+
+    attr_reader :data, :fd
+
+    private
+
+    def initialize(pid)
+      @pid = pid
+
+      @data = get_data
+      @fd = get_fd
+    end
+
+
+    def get_data
+      %x{ cat /proc/#{@pid}/status }.split("\n")
+    end
+
+    def get_fd
+      %x{ echo 123 | sudo -S ls -l /proc/#{@pid}/fd }.split("\n")
+                                                     .drop(1)
+                                                     .map { _1.split('->') }
+                                                     .map { [_1[0].split(' ').last, _1[1].strip] }
+                                                     .to_h
+    end
+  end
+end
