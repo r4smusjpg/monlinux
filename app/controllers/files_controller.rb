e9589eab7f8654c5f39f2be8d41ff1f7de9e2062
r4smusjpg <r4smusjpg@gmail.com>
2024-04-13 16:43:00 +0300

    refactor FilesController a bit

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,10 +1,12 @@
 class FilesController < ApplicationController
   def show
     path = URI.decode_uri_component(params[:path].gsub('*', '.'))
-    @data = prepare_data(%x{ stat #{path} })
+    @data = get_data(path)
   end
 
-  def prepare_data(data)
-    data.gsub(/ +/, ' ').split(/[\n\t]/).map(&:strip)
+  def get_data(path)
+    %x{ stat #{path} }.gsub(/ +/, ' ')
+                      .split(/[\n\t]/)
+                      .map(&:strip)
   end
 end
