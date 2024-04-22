5b2fb635b1654c7f352dfb1a867cf3c733d0d464
r4smusjpg <r4smusjpg@gmail.com>
2024-04-22 13:33:04 +0300

    refactor file stat functionality

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,6 +1,6 @@
 class FilesController < ApplicationController
   def show
     path = URI.decode_uri_component(params[:path].gsub('*', '.'))
-    @stat_buf = Files::StatService.call(path)
+    @stat_buf = File.stat(path)
   end
 end
