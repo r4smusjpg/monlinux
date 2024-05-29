eff79e1845d0d923c208271884a13c67810595fc
r4smusjpg <r4smusjpg@gmail.com>
2024-05-29 19:05:53 +0300

    add info about device to inode data

--- app/controllers/inodes_controller.rb
+++ app/controllers/inodes_controller.rb
@@ -2,6 +2,8 @@ class InodesController < ApplicationController
   def show
     inode = params[:inode]
     device_numbers = params[:device_numbers]
-    @data = ::Inodes::DataService.call(inode, device_numbers)
+    service = ::Inodes::DataService.call(inode, device_numbers)
+    @data = service.data
+    @device_name = service.device_name
   end
 end
--- app/services/inodes/data_service.rb
+++ app/services/inodes/data_service.rb
@@ -1,14 +1,26 @@
 module Inodes
   class DataService
     def self.call(inode, device_numbers)
-      new.get_data(inode, device_numbers)
+      new(inode, device_numbers)
     end
 
-    def get_data(inode, device_numbers)
-      mount_name = %x{ lsblk | grep #{device_numbers}}&.split(' ')[0]
-                                                      &.slice(/\w+/)
-      %x{ echo "stat <#{inode}>" | debugfs /dev/#{mount_name} }.split("\n")
-                                                               .reject { _1 =~ /debugfs/}
+    attr_reader :device_name, :data
+
+    private
+
+    def initialize(inode, device_numbers)
+      @device_name = get_device_name(device_numbers)
+      @data = get_data(inode)
+    end
+
+    def get_device_name(device_numbers)
+      %x{ lsblk | grep #{device_numbers}}&.split(' ')[0]
+                                         &.slice(/\w+/)
+    end
+
+    def get_data(inode)
+      %x{ echo "stat <#{inode}>" | debugfs /dev/#{@device_name} }.split("\n")
+                                                                 .reject { _1 =~ /debugfs/}
     end
   end
 end
