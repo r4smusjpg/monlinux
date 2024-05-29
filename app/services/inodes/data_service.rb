7ca03e28b917e62d998acce47ebb481234f4d723
r4smusjpg <r4smusjpg@gmail.com>
2024-05-29 19:08:28 +0300

    fix

--- app/services/inodes/data_service.rb
+++ app/services/inodes/data_service.rb
@@ -14,12 +14,13 @@ module Inodes
     end
 
     def get_device_name(device_numbers)
-      %x{ lsblk | grep #{device_numbers}}&.split(' ')[0]
-                                         &.slice(/\w+/)
+      temp_name = %x{ lsblk | grep #{device_numbers}}&.split(' ')[0]
+                                                     &.slice(/\w+/)
+      %x{ df  | grep #{temp_name} }.split(' ')[0]
     end
 
     def get_data(inode)
-      %x{ echo "stat <#{inode}>" | debugfs /dev/#{@device_name} }.split("\n")
+      %x{ echo "stat <#{inode}>" | debugfs #{@device_name} }.split("\n")
                                                                  .reject { _1 =~ /debugfs/}
     end
   end
